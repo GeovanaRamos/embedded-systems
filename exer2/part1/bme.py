99aabef028637e0f6dccb273f547d19480ea1a3f
GeovanaRamos <geovannna.1998@gmail.com>
2020-09-17 08:51:32 -0300

    Adding exer2 part 1

---
+++ exer2/part1/bme.py
@@ -0,0 +1,19 @@
+import smbus2
+import bme280
+import RPi_I2C_driver
+from time import sleep
+
+mylcd = RPi_I2C_driver.lcd()
+
+porta_i2c = 1
+endereco = 0x76
+bus = smbus2.SMBus(porta_i2c)
+
+calibracao_paramentros = bme280.load_calibration_params(bus, endereco)
+dado = bme280.sample(bus, endereco, calibracao_paramentros)
+
+for i in range(10):
+    sleep(1)
+    mylcd.lcd_display_string("GRSS" + str(i) + " T:" +'{0:.2f}'.format(dado.temperature), 1)
+    mylcd.lcd_display_string("U:" +'{0:.2f}'.format(dado.humidity) + " P:" + '{0:.2f}'.format(dado.pressure), 2)
+
